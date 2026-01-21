033ba66a6478a9dadee816a18da3facfb61754ad
Pedro Sergey <pedrosergey.datascientist@gmail.com>
2026-01-21 15:22:20 +0000

    finishing docker

--- 01-docker-terraform/notes.md
+++ 01-docker-terraform/notes.md
@@ -1,8 +1,10 @@
-**Docker**
+# **Docker**
 
 Docker is "stateless", so everytime that we run a Docker Image, we will get the same result. It does not matter how many changes we do while running the container, once we exit it, all the changes will be discarded. In order to this to change, we need to to modify the Docker Image.
 
 This is not 100% true, but it is how is suposed to be. 
+
+## 1. Basic commands
 
 - Most basic docker command:
 `docker run hello-world`
@@ -18,10 +20,71 @@ This is not 100% true, but it is how is suposed to be.
 - Get the code of the exited containers: `docker ps -aq`
 - Remove those containers: `docker rm 'docker ps -aq'`
 
-**Python**
+
+
+## 2. Advanced commands:
+
+In order to use data from the host in the container, volumes could be used. This can be done using the -v flag, and then host_directory:container_directory
+
+`docker run -it --entrypoint=bash -v $(pwd)/test:/app/test python:3.13.11-slim`
+
+Example to run a command and create something to a local folder: `docker run -it -v $(pwd)/external:/../external test:pandas`
+
+If we want to preserve some data internally in the container, we also could do something like `-v internal_volume:/internal_volume`
+
+## 3. Create containers:
+
+- Create a docker container (with the -t flag to give it the name): `docker build -t test:pandas .`
+- After the container is created, it could be runned with: `docker run test:pandas`
+- In order to make the image stateless, we need to add the `--rm` flag when running the container (it is highly recommended).
+
+## 4. POSTGRES
+
+A basic command to run a postgres container:
+```
+docker run -it --rm \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  postgres:18
+  ```
+
+Connect to the database using pgadmin: `uv run pgcli -h localhost -p 5432 -u root -d ny_taxi`
+
+In order to many containers interact with each other, a network should be created: `docker network create`. And then use this network to communicate the containers `--network=...`. It is important to give names to the containers, in order to set later the host.
+
+### 4.1 PGADMIN
+
+To connect to a proper interface, we could use another container (remember to add the newtork tag):
+
+```
+docker run -it \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -v pgadmin_data:/var/lib/pgadmin \
+  -p 8085:80 \
+  --network=pg-network \
+  dpage/pgadmin4
+```
+
+# **Python**
 
 - Version: `python -V` or `python --version`
+- To pass args to the file: `import sys` & `sys.argv`
 
-**BASH**
+## Virtual environment (VE):
+
+- Install the uv, to manage VE: `pip install uv`
+- Create a VE: `uv init --python 3.13`
+- Run commands in the VE: `uv run python -V`
+- Add dependencies to the VE (the --dev flag could be use to add only dependencies to the dev part): `uv add pandas pyarrow`
+- Create a jupyter notebook sesion: `uv run jupyter notebook`
+- Convert jupyter notebook into a python script: `uv run jupyter nbconvert --to=script notebook.ipynb`
+
+# **BASH**
 
 - To install packages, first need to run `apt update`, and then `apt instal ...`
+
+- Change the folder path: `PS1="> "`
--- 01-docker-terraform/pipeline/ingest_data.py
+++ 01-docker-terraform/pipeline/ingest_data.py
@@ -1,109 +1,88 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
-
-
 from sqlalchemy import create_engine
 import pandas as pd
 from tqdm.auto import tqdm
+import click
 
+@click.command()
+@click.option('--year', default=2021, help='Year of the data')
+@click.option('--month', default=1, help='Month of the data')
+@click.option('--chunk-size', default=100000, help='Chunk size for reading CSV')
+@click.option('--user', default='root', help='PostgreSQL username')
+@click.option('--password', default='root', help='PostgreSQL password')
+@click.option('--host', default='localhost', help='PostgreSQL host')
+@click.option('--port', default=5432, help='PostgreSQL port')
+@click.option('--db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--table', default='yellow_taxi_data', help='Table name for data')
 
-# In[26]:
+def run(year, month, chunk_size, user, password, host, port, db, table):
 
+    # use the data types to enhance the performance
 
-# create the url to download
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+    }
 
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-file = 'yellow_tripdata_2021-01.csv.gz'
-url = prefix + file
-
-
-# In[27]:
-
-
-# use the data types to enhance the performance
-
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
+    parse_dates = [
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
 ]
 
-
-# In[33]:
-
-
-# check the schema that will be created 
-
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    nrows=1
-)
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+   
 
 
-# In[29]:
+    # create the conextion to the database
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
 
-# create the conextion to the database
-
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[37]:
-
-
-# add the schema to the database
-
-df.head(0).to_sql(
-    'ny_taxi_data',
-    con=engine,
-    if_exists='replace'
-)
+    df = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        nrows=0
+    )
 
 
-# In[38]:
+    df_iterator = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize = chunk_size
+    )
+    
+    first = True
 
+    for iterator in tqdm(df_iterator):
+        if first:
+            iterator.to_sql(table, con=engine, if_exists='replace'
+        )
+            first = False
+        else:
+            iterator.to_sql(table, con=engine, if_exists='append'
+        )
 
-# create the iterator
+    print("Ingestion complete! :)")
 
-df_iterator = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize = 100000
-)
-
-
-# In[40]:
-
-
-# add the data to the database
-
-for iterator in tqdm(df_iterator):
-    iterator.to_sql('ny_taxi_data', con=engine, if_exists='append'
-)
-
+if __name__ == '__main__':
+    run()
